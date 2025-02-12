c4a4bf911018d2da92866fe0e9b9913f85df2be9
bananijohn <bananijohn@gmail.com>
2025-02-12 13:30:23 +0100

    still fixing lix...

--- flake.nix
+++ flake.nix
@@ -104,8 +104,8 @@ in
     # $ darwin-rebuild build --flake .#coolflake
     darwinConfigurations."coolflake" = nix-darwin.lib.darwinSystem {
       modules = [
-	configuration
 	lix-module.darwinModules.default
+	configuration
 	nix-homebrew.darwinModules.nix-homebrew
 	{
 	  nix-homebrew = {
