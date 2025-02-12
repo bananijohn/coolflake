b2e87c96cd30ffd0c504e92b8e155207ab5699a9
bananijohn <bananijohn@gmail.com>
2025-02-12 14:39:45 +0100

    changed dock a lil

--- flake.nix
+++ flake.nix
@@ -67,6 +67,7 @@ in
 
       system.defaults = {
         dock.autohide = true;
+	dock.autohide-time-modifier = 0.3
 	WindowManager.EnableTopTilingByEdgeDrag = true;
 	finder._FXShowPosixPathInTitle = true;
 	finder.QuitMenuItem = true;
