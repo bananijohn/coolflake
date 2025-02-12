ddf443722c8f4f8d3f72e46f6aa50bb362823ef8
bananijohn <bananijohn@gmail.com>
2025-02-12 10:43:55 +0100

    fixed the syntax error

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs"
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zahofengli-wip/nix-homebrew";
   };
 
