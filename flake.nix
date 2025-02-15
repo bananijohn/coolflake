d684fbb40cd2b4c3b9d6e2368e8711e463b07314
bananijohn <bananijohn@gmail.com>
2025-02-15 21:01:42 +0100

    added new things

--- flake.nix
+++ flake.nix
@@ -6,30 +6,41 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nixvim, nix-homebrew }:
   let
     configuration = { pkgs, config, ... }: {
       # Installed packages. to search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [ 
-	  pkgs.neovim
+	  pkgs.neovim-unwrapped
+	  pkgs.skhd
 	  pkgs.kitty
+	  pkgs.cava
 	  pkgs.mkalias
+	  pkgs.cmatrix
+	  pkgs.yazi
+	  pkgs.gh
 	  pkgs.git
 	  pkgs.home-manager
-	  pkgs.oh-my-zsh
 	  pkgs.vesktop
 	  pkgs.kitty-img
 	  pkgs.kitty-themes
 	  pkgs.yabai
-	  pkgs.skhd
 	  pkgs.fastfetch
 	  pkgs.sketchybar
 	  pkgs.sketchybar-app-font
+	  pkgs.jq
+	  pkgs.starship
+	  pkgs.stow
 	];
+
+	# Zsh syntax highlighting
+
 
 	homebrew = {
 	  enable = true;
@@ -37,6 +48,12 @@
 	  brews = [
 	  ];
 	  casks = [
+	    "typeface"
+	    "font-symbols-only-nerd-font"
+	    "font-proggy-clean-tt-nerd-font"
+	    "background-music"
+	    "desktoppr"
+	    "font-fira-code-nerd-font"
 	  ];
 	  taps = [
 	  ];
@@ -68,8 +85,10 @@ in
 
       system.defaults = {
         dock.autohide = true;
-	dock.autohide-time-modifier = 0.3;
+	dock.autohide-time-modifier = 0.1;
 	dock.autohide-delay = 0.05;
+	dock.minimize-to-application = true;
+	dock.show-recents = false;
 	WindowManager.EnableTopTilingByEdgeDrag = true;
 	finder._FXShowPosixPathInTitle = true;
 	finder.QuitMenuItem = true;
@@ -82,33 +101,49 @@ in
       # Yabai config ig lmao
 
       services.yabai = {
-        enable = false;
+        enable = true;
 	enableScriptingAddition = true;
-	config = {
-	  window_placement = "second_child";
-	  top_padding = 5;
-	  bottom_padding = 5;
-	  left_padding = 5;
-	  right_padding = 5;
-	  window_gap = 5;
-	  auto_balance = "off";
-	  split_ratio = 5;
-	  layout = "bsp";
-	  mouse_modifier = "command";
-	  mouse_action1 = "move";
-	  mouse_action2 = "resize";
-	  window_shadow = "float";
-	  window_opacity = "on";
-	  active_window_opacity = "0.9";
-	  normal_window_opacity = "0.7";
-        };
+#	config = {
+#	  window_placement = "second_child";
+#	  focus_follows_mouse = "autofocus";
+#	  mouse_follows_focus = "on";
+#	  top_padding = 5;
+#	  bottom_padding = 5;
+#	  left_padding = 5;
+#	  right_padding = 5;
+#	  window_gap = 5;
+#	  auto_balance = "on";
+#	  split_ratio = 5;
+#	  layout = "bsp";
+#	  mouse_modifier = "cmd";
+#	  mouse_action1 = "move";
+#	  mouse_action2 = "resize";
+#	  window_shadow = "float";
+#	  window_opacity = "on";
+#	  active_window_opacity = "0.95";
+#	  normal_window_opacity = "0.9";
+#        };
+#	extraConfig = "yabai -m space --create && yabai -m config external_bar all:37:0";
       };
 
       # skhd config lol
 
       services.skhd = {
-        enable = false;
+        enable = true;
     };
+      
+      # Zsh config :D
+
+      programs.zsh = {
+	  enableFastSyntaxHighlighting = true;
+	};
+
+      # Sketchybar config
+
+      services.sketchybar = {
+        enable = true;
+    };
+
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
@@ -129,6 +164,25 @@ in
     darwinConfigurations."coolflake" = nix-darwin.lib.darwinSystem {
       modules = [
 	configuration
+	nixvim.nixDarwinModules.nixvim
+	{
+	  programs.nixvim = {
+	    enable = true;
+	    plugins.lualine.enable = true;
+	    plugins.treesitter.enable = true;
+	    colorschemes.catppuccin = {
+	      enable = true;
+	      autoLoad = true;
+	      settings = {
+	        flavour = "mocha";
+		integrations = {
+		  lualine.nvim = true;
+		  treesitter = true;
+		};
+	    };
+	  };
+        };
+      }
 	nix-homebrew.darwinModules.nix-homebrew
 	{
 	  nix-homebrew = {
